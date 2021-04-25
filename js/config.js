3ca934e6f29cbda3f29b588e8efef3b564def232
Zayra <https://github.com/j0i>
2021-04-24 20:11:31 -0500

    added pool, client and region

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_irQc2cCca', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'b4hkc1ehq7nimhiejdmd6it76', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
