580fc8bcd57ba9d2ee034547a3c7b47cebb3813f
Zayra <https://github.com/j0i>
2021-04-25 20:48:58 -0500

    deleted some comments

--- js/vendor/rocket.js
+++ js/vendor/rocket.js
@@ -1,14 +1,11 @@
 var launch = document.getElementsByClassName("launch"),
     rocket = document.getElementsByClassName("rocket");
-    /*footer = document.getElementsByTagName("footer");*/
 
 launch[0].addEventListener("click", function () {
   launch[0].style.display = "none";
   rocket[0].style.animation = "fire 20s";
-  /*footer[0].style.opacity = 1;*/
   setTimeout(function () {
     rocket[0].style.animation = "";
     launch[0].style.display = "block";
-    /*footer[0].style.opacity = 0;*/
   }, 20000)
 });
